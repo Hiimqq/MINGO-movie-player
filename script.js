afe84ae84662bad01e4a57f5f9dcbcfb4e825d4b
Alex Carmody <alex-carmody@live.com>
2022-07-03 21:11:04 -0400

    added progressBar functionality, updated DOM selectors

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.querySelector('video');
-const progressRange = document.querySelector('progress-range');
-const progressBar = document.querySelector('progress-bar');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
 const playBtn = document.getElementById('play-btn');
 const volumeIcon = document.getElementById('volume-icon');
-const volumeRange = document.querySelector('volume-range');
-const volumeBar = document.querySelector('volume-bar');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
@@ -33,7 +33,11 @@ video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Update progressBar as video plays
 
+function updateProgress() {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+}
 
 // Volume Controls --------------------------- //
 
@@ -49,3 +53,5 @@ video.addEventListener('ended', showPlayIcon);
 // Event listeners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
